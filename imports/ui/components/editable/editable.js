7c6c5f27221e485c3bf4d73cc7d284328876f2f4
lidor cohen <lidorcg@gmail.com>
2016-12-19 08:43:37 +0200

    - separate editable from link-entry

---
+++ imports/ui/components/editable/editable.js
@@ -0,0 +1,46 @@
+import { Template } from 'meteor/templating';
+
+import './editable.html';
+
+Template.editable.onCreated(function() {
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    editing: false,
+  });
+});
+
+Template.editable.helpers({
+  editing() {
+    return Template.instance().state.get('editing');
+  },
+  onDoneEditing () {
+    const state = Template.instance().state
+    return () => {
+      state.set('editing', false);
+    }
+  }
+});
+
+Template.editable.events({
+  'dblclick .editable' (event, instance) {
+    instance.state.set('editing', true);
+  },
+});
+
+Template.editable_form.events({
+  'submit form' (event) {
+    event.preventDefault();
+    data = Template.currentData();
+    let fields = {};
+    fields[data.name] = event.target.input.value;
+    data.item.update(fields);
+    this.doneEditing();
+  },
+  'blur input' () {
+    this.doneEditing();
+  },
+});
+
+Template.editable_form.onRendered(function() {
+  Template.instance().$('input').focus();
+});
